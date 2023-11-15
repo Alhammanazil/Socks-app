243f9f03b8d3284d95e5f6fe98b08fefa7d8440d
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2023-11-15 19:37:20 +0700

    add description product

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            object: 'Step into style and comfort with our latest Shoes. Modern design, premium materials, and versatile for any occasion. Upgrade your footwear game today!'
+        }
+    }
+})
