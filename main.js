ef7b6485d4c29b0c7c219379acd5bea1e905b836
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2023-11-15 19:57:48 +0700

    change data object

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            object: 'Step into style and comfort with our latest Shoes. Modern design, premium materials, and versatile for any occasion. Upgrade your footwear game today!'
+            description: 'Step into style and comfort with our latest Shoes. Modern design, premium materials, and versatile for any occasion. Upgrade your footwear game today!'
         }
     }
 })
