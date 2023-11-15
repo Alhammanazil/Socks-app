f4b531a435d0755cd5fe58def3c130434277b76f
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2023-11-15 19:52:08 +0700

    add url to the data object

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            object: 'https://www.google.com/shopping/product/1?q=socks&prds=epd:7551922826790336225,eto:7551922826790336225_0,pid:7551922826790336225&sa=X&ved=0ahUKEwiYle7ihMaCAxXD2TgGHbReDMoQ9pwGCAU'
         }
     }
 })
