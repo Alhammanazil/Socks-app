2ce95c2e30932db773f252b4b964064380260fbb
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2023-11-15 19:59:11 +0700

    change data object

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            object: 'https://www.google.com/shopping/product/1?q=socks&prds=epd:7551922826790336225,eto:7551922826790336225_0,pid:7551922826790336225&sa=X&ved=0ahUKEwiYle7ihMaCAxXD2TgGHbReDMoQ9pwGCAU'
+            url: 'https://www.google.com/shopping/product/1?q=socks&prds=epd:7551922826790336225,eto:7551922826790336225_0,pid:7551922826790336225&sa=X&ved=0ahUKEwiYle7ihMaCAxXD2TgGHbReDMoQ9pwGCAU'
         }
     }
 })
